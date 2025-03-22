65ef6d11b8333e5ea76c1dc15398ce17be90c859
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2025-03-22 10:36:30 -0700

    add mod_audio_stream deps and installation

--- install-fs.sh
+++ install-fs.sh
@@ -86,3 +86,24 @@ git clone https://github.com/MikeB1124/freeswitch.git /tmp/freeswitch \
     && make && make install \
     && make sounds-install \
     && make moh-install
+
+
+#Install Freeswitch Deps and mod_audio_stream
+TOKEN=pat_8s48w5HSCYdH8gBrLmzYsXiX
+
+apt-get update && apt-get install -yq gnupg2 wget lsb-release -y
+wget --http-user=signalwire --http-password=$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
+
+echo "machine freeswitch.signalwire.com login signalwire password $TOKEN" > /etc/apt/auth.conf
+chmod 600 /etc/apt/auth.conf
+echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
+echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
+
+apt-get update -y
+ 
+# Install dependencies required for the build
+apt-get build-dep freeswitch -y
+
+wget https://github.com/amigniter/mod_audio_stream/releases/download/v1.0.0/mod-audio-stream_1.0.0_amd64.deb
+dpkg -i mod-audio-stream_1.0.0_amd64.deb
+mv /usr/lib/freeswitch/mod/mod_audio_stream.so /usr/local/lib/freeswitch/mod/
